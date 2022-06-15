dbed194e7726eb4b1ac1a282fbe83bfc552a9d1d
Romual Naibaho <74808767+romualnaibaho@users.noreply.github.com>
2022-06-15 13:11:16 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
